b7310cfbbd589770aa6ab645a847f811966c9f2f
Elisha Witte <elisha@ewitte.me>
2016-10-05 18:02:50 +0200

    Added video to AMV fillables.

--- app/AMV.php
+++ app/AMV.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class AMV extends Model
 {
     protected $table = 'amvs';
-    protected $fillable = ['title', 'genre', 'animes', 'music', 'description', 'poster', 'bg', 'user_id'];
+    protected $fillable = ['title', 'genre', 'animes', 'music', 'description', 'poster', 'bg', 'video', 'user_id'];
 
     public function contests() {
         return $this->belongsToMany('App\Contest', 'amv_contest', 'amv_id', 'contest_id')
