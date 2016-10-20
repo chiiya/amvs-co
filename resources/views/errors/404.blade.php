87f6436e5972a07d737af75adc184555cb6a9f7a
Elisha Witte <elisha@ewitte.me>
2016-10-20 21:32:13 +0200

    Custom 404 page.

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,25 @@
+@extends('layouts.app')
+
+@section('title', '404 - Not Found')
+
+@section('content')
+    <div class="container">
+        <img class="img-responsive center-block" src="/img/404.png">
+        <div class="wrapper">
+            <h1>Sorry, the requested page could not be found.</h1>
+            @if ($exception)
+            <pre>{{ $exception }}</pre>
+            @endif
+            <p>Maybe one of these topics could be useful:</p>
+            <ul>
+                <li><a href="/users">User Overview</a></li>
+                <li><a href="/amvs">AMV Overview</a></li>
+                <li><a href="/contests">Current Contests</a></li>
+            </ul>
+        </div>
+    </div>
+@endsection
+
+@section('scripts')
+  @parent
+@endsection
